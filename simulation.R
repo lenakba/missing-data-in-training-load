c3c72c9dc16bbb151b68b9622182f29d3c41bd9a
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-02-11 18:16:50 +0100

    MAde function for adding missing completely at random with a choosable proportion missing.

--- simulation.R
+++ simulation.R
@@ -15,5 +15,22 @@ d_td_full = d_full %>% select(p_id, training_date, day_of_week, total_distance_d
 d_srpe_full = d_full %>% select(p_id, training_date, day_of_week, load)
 
 # now we remove missing until we have a dataset where we know 100% what the values are
-d_td = d_td_full %>% filter(!is.na(load))
-d_srpe = d_srpe_full %>% filter(!is.na(load))
+d_td = d_td_full %>% filter(!is.na(load)) %>% rownames_to_column()
+d_srpe = d_srpe_full %>% filter(!is.na(load)) %>% rownames_to_column()
+
+set.seed(123)
+add_mcar = function(d, missing_prop){
+  n_values = nrow(d)
+  random_spots = sample(1:n_values, round(missing_prop*n_values))
+  d = d %>% mutate(load = ifelse(rowname %in% random_spots, NA, load))
+  d
+}
+
+d_td_005 = add_mcar(d_td, 0.05)
+d_td_01 = add_mcar(d_td, 0.1)
+d_td_08 = add_mcar(d_td, 0.8)
+
+d_td_005 %>% summarise(missing = sum(is.na(load)), n = n(), prop = missing/n)
+d_td_01 %>% summarise(missing = sum(is.na(load)), n = n(), prop = missing/n)
+d_td_08 %>% summarise(missing = sum(is.na(load)), n = n(), prop = missing/n)
+
