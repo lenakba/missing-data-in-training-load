a1e76d7f077c72f899318481e57ec8574b386f6f
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-02-11 15:27:19 +0100

    created two datasets, one with total distance and one with sRPE

--- simulation.R
+++ simulation.R
@@ -5,5 +5,13 @@ library(tidyverse) # for datawrangling
 
 # reading data
 folder_data = paste0("O:\\Prosjekter\\Bache-Mathiesen-002-missing-data\\Data\\")
-read_delim(paste0(folder_data, "norwegian_premier_league_football_anon.csv"), delim = ";")
+d_full = read_delim(paste0(folder_data, "norwegian_premier_league_football_anon.csv"), delim = ";")
+
+# dividing into two datasets
+# one where no one player and day is missing total distance
+# one where no one player and day  is missing srpe
+# then we rename the td and srpe variable to the same name "load"
+# because we want to use functions later on without having to differentiate between total distance and srpe later on
+d_td = d_full %>% select(p_id, training_date, total_distance_daily) %>% rename(load = total_distance_daily)
+d_srpe = d_full %>% select(p_id, training_date, load)
 
