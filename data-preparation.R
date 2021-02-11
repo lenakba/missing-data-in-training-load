fe433afbca71e295e72cc9037628ddb6aef313e2
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-02-11 16:13:27 +0100

    Fixed problem with players who reported having trained, with a certain duration that wasnt 0, and yet set RPE to be 0, when 0 means no training. In addition, added duration and difficulty to rpe dataset

--- data-preparation.R
+++ data-preparation.R
@@ -14,6 +14,7 @@
 library(DBI) # for database extraction with SQL
 library(tidyverse) # for data wrangling
 library(rlang) # tidyverse function building tools
+library(chron) # for working with time variables
 
 #optional packages
 library(devEMF) # for saving emf files
@@ -105,10 +106,37 @@ d_td_daily %>% arrange(desc(total_distance_minute))
 d_srpe_full = dbGetQuery(db_stromsgodset, 
                          paste0("SELECT *
                   FROM training_data_2019.temp_training_log")) %>% as_tibble() 
-d_srpe = d_srpe_full %>% select(player_id, training_date = planned_date, activity, duration, difficulty, load)
+d_srpe_selected = d_srpe_full %>% select(player_id, training_date = planned_date, activity, duration, difficulty, load)
+
+# test that no one has 0 duration or 0 diffulty yet have more than 0 load or difficulty
+d_srpe_selected %>% filter(duration == "00:00:00")
+d_srpe_selected %>% filter(difficulty == 0 & activity == "Training") 
+
+# some players have reported the activity "training" with a duration higher than 0, 
+# yet difficulty is set to 0, which means no training was performed
+# we will assume these had a difficulty of 1
+# in addition, if activity = training, but duration = 0, we assume difficulty and load to be 0 too
+# we use the chron package to create duration into a numeric variable
+d_srpe_selected = d_srpe_selected %>% 
+                  mutate(times_tz = chron::times(duration), 
+                         minutes = chron::minutes(times_tz), 
+                         hours = chron::hours(times_tz),
+                         hours_in_minutes = hours*60,
+                         minutes_sum = hours_in_minutes+minutes,
+                         duration_min = minutes_sum,
+                         difficulty = ifelse(activity == "Training" & difficulty == 0 & duration_min > 0, 1, difficulty),
+                         difficulty = ifelse(activity == "Training" & duration_min == 0, 0, difficulty),
+                         load = duration_min*difficulty) %>% # we need to recalculate srpe thanks to that
+  select(player_id, training_date, difficulty, duration = duration_min, load)
 
 # we sum multiple sessions on the same day per individual for a daily sRPE measure
-d_srpe = d_srpe %>% group_by(player_id, training_date) %>% summarise(load = sum(load, na.rm = TRUE))
+d_srpe_daily = d_srpe_selected %>% group_by(player_id, training_date) %>% summarise(load = sum(load, na.rm = TRUE))
+
+# in case we need them, we choose a random difficulty and duration row per player, per date and add it to the daily srpe data
+# daily duration and difficulty (ddd)
+# the duration and difficulty wont always multiply to the load because multiple sessions may have been part of the srpe and here we just chose one
+d_ddd = d_srpe_selected %>% select(player_id, training_date, duration, difficulty) %>% distinct(player_id, training_date, .keep_all = TRUE) 
+d_srpe = d_srpe_daily %>% left_join(d_ddd, by = c("player_id", "training_date"))
 
 #--------------------------------------------------------------------- Step 5: Find implicit days where the players did not have any training (day after match, weekends etc.)
 
