7826da29e34adf031530b97255f80c28087a5ffb
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-02-17 13:21:58 +0100

    Fetch daily total distance data with sRPE as additional informative variable, and rpe and duration data at the training session level. We will use both datasets in the simulations.

--- simulation-mar.R
+++ simulation-mar.R
@@ -4,19 +4,16 @@ library(tidyverse) # for datawrangling
 
 # reading data
 folder_data = paste0("O:\\Prosjekter\\Bache-Mathiesen-002-missing-data\\Data\\")
-d_full = read_delim(paste0(folder_data, "norwegian_premier_league_football_anon.csv"), delim = ";")
 
-# dividing into two datasets
-# one where no one player and day is missing total distance
-# one where no one player and day is missing srpe
-# then we rename the td and srpe variable to the same name "load"
-# because we want to use functions later on without having to differentiate between total distance and srpe later on
-d_td_full = d_full %>% select(p_id, training_date, day_of_week, total_distance_daily) %>% rename(load = total_distance_daily)
-d_srpe_full = d_full %>% select(p_id, training_date, day_of_week, load)
+# note that the Total Distance data is per day, the RPE data is per session (which there can be multiple of per day)
+d_td_full = read_delim(paste0(folder_data, "norwegian_premier_league_football_td_anon.csv"), delim = ";")
+d_rpe_full = read_delim(paste0(folder_data, "norwegian_premier_league_football_rpe_anon.csv"), delim = ";")
 
-# now we remove missing until we have a dataset where we know 100% what the values are
-d_td = d_td_full %>% filter(!is.na(load)) %>% rownames_to_column()
-d_srpe = d_srpe_full %>% filter(!is.na(load)) %>% rownames_to_column()
+# remove missing
+# select vars we need in the simulation, daily vars we think affect the level of sRPE and total distance
+infovars = c("p_id", "training_date", "n_matches", "mc_day")
+d_td = d_td_full %>% filter(!is.na(total_distance_daily)) %>% select(all_of(infovars), td = total_distance_daily, srpe) 
+d_srpe = d_rpe_full %>% filter(!is.na(rpe) & !is.na(duration)) %>% select(all_of(infovars), rpe, duration)
 
 # vector of chosen missing proportions
 missing_prop_v = c(0.05, 0.1, 0.2, 0.4, 0.6, 0.8)
