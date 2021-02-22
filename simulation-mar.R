17d32d4007f6a239bc8e806bf35cf408f58e955b
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-02-22 10:57:31 +0100

    Moved everything up so that a) we know exactly what functions etc. are needed for the simulation and b) so the simulation can be run without accidentally running something that isnt needed

--- simulation-mar.R
+++ simulation-mar.R
@@ -25,7 +25,178 @@ d_srpe = d_rpe_full %>% filter(!is.na(rpe) & !is.na(duration)) %>% select(all_of
 # we have all functions needed for performing the simulation in this section
 # if you want to look at each step in the simulation, please start at the section Step 1
 
+# All the functions we need are listed first
 
+# logistic function
+log_reg = function(tl_coef){
+  res = 1 / (1 + exp(-tl_coef))
+  res
+}
+
+# linear logistic regression function
+# where the level of sRPE effects injury probability
+inj_probability_srpe = function(srpe){
+  y = log_reg(-2 + 0.003*srpe) 
+  y
+}
+
+# Create missing completely at random
+add_mcar_rpe = function(d, missing_prop){
+  n_values = nrow(d)
+  d = d %>% rownames_to_column()
+  random_spots_rpe = sample(1:n_values, round(missing_prop*n_values))
+  random_spots_min = sample(1:n_values, round(missing_prop*n_values))
+  d = d %>% mutate(rpe = ifelse(rowname %in% random_spots_rpe, NA, rpe),
+                   duration = ifelse(rowname %in% random_spots_min, NA, duration)) %>% dplyr::select(-rowname)
+  d
+}
+
+# Create missing with Missing probability based on other variables (Missing at Random)
+# linear logistic regression function
+# for the relationship between different variables and the probability of missing
+mar_function = function(d, corr){
+  if(corr == "light"){
+    y = log_reg(-2 + (0.03*d$age) + (0.02*d$sex) + (0.3*d$freeday))
+  } else if(corr == "medium"){
+    y = log_reg(-2 + (0.08*d$age) + (0.04*d$sex) + (0.8*d$freeday))    
+  }  else if(corr == "strong"){
+    y = log_reg(-2 + (0.13*d$age) + (0.1*d$sex) + (1.8*d$freeday) + (1.8*d$match))     
+  }
+  y
+}
+
+# adding missing at random to a dataset
+add_mar_rpe = function(d, corr){
+  d = d %>% mutate(na_prop = mar_function(., corr),
+                   na_spot_rpe = rbinom(length(na_prop), 1, prob = na_prop),
+                   na_spot_duration = rbinom(length(na_prop), 1, prob = na_prop),
+                   rpe = ifelse(na_spot_rpe == 1, NA, rpe),
+                   duration = ifelse(na_spot_duration == 1, NA, duration))
+  d %>% dplyr::select(-starts_with("na"), -freeday, -match)
+}
+
+# impute mean function which calculates the mean by a chosen grouping variable
+impute_mean = function(d, group_var){
+  group_var = enquo(group_var)
+  
+  # calc mean
+  d_imp = d %>% 
+    group_by(!!group_var) %>% 
+    mutate(m_rpe = mean(rpe, na.rm = TRUE),
+           m_duration = mean(duration, na.rm = TRUE)) %>% 
+    ungroup()
+  
+  # impute with the mean
+  d_imp = d_imp %>% mutate(rpe = ifelse(is.na(rpe), m_rpe, rpe),
+                           duration = ifelse(is.na(duration), m_duration, duration),
+                           srpe = rpe*duration) %>% 
+    dplyr::select(-m_rpe, -m_duration)
+  d_imp
+}
+
+# fit our models
+fit_glm = function(d){
+  fit = glm(injury ~ srpe, family = "binomial", data = d)
+  fit
+}
+
+# function for obtaining parameters from any model fit
+# specify method for a column with the model-type
+# and whether its based on a mids object or not
+get_params = function(fit, method, pool = FALSE){
+  if(pool){
+    d_params = summary(mice::pool(fit), "all", conf.int = TRUE) %>% 
+      dplyr::select(term, estimate, CI_low = "2.5 %", CI_high = "97.5 %", p = p.value) %>% 
+      as_tibble() %>% 
+      mutate(term =  as.character(term))
+  } else {
+    d_params = parameters::parameters(fit) %>% tibble() %>% 
+      dplyr::select(term = Parameter, estimate = Coefficient, CI_low, CI_high, p)
+  }
+  d_params = d_params %>% mutate(method = method) 
+  d_params
+}
+
+# The final, helper function that performs all the imputations at once, given a dataset with missing
+# then, it fits a logistic regression model on the data
+# and outputs the needed model parameters for the validation of the imputation models
+sim_impfit = function(d_missing, target_param, rep = 1){
+  
+  #-----------------impute using all the different methods
+  # Mean imputation by the mean per player
+  d.mean.p_id = impute_mean(d_missing, p_id)
+  
+  # Mean imputation by the mean per week
+  d.mean.week = impute_mean(d_missing, week_nr)
+  
+  # Regression imputation
+  mids.reg = mice(d_missing, method = "norm.predict", seed = 1234, m = 1, print = FALSE)
+  d.reg = mice::complete(mids.reg, "long", include = FALSE) %>% mutate(srpe = rpe*duration)
+  
+  # Multiple imputation with predicted mean matching, and the Impute, then transform strategy
+  mids.pmm = mice(d_missing, print = FALSE, seed = 1234)
+  imp.pmm = mice::complete(mids.pmm, "long", include = TRUE)
+  imp.pmm$srpe = with(imp.pmm, rpe*duration)
+  mids.itt.pmm = as.mids(imp.pmm)
+  
+  # complete case analysis
+  d.cc = na.omit(d_missing) %>% mutate(srpe = rpe*duration)
+  
+  # fit our models
+  fit.mean.p_id = fit_glm(d.mean.p_id)
+  fit.mean.week = fit_glm(d.mean.week)
+  fit.reg =  fit_glm(d.reg)
+  fit.pmm =  with(mids.itt.pmm, glm(injury ~ srpe, family = binomial))
+  fit.cc =  fit_glm(d.cc)
+  
+  # fetch model parameters
+  tab1 = get_params(fit.mean.p_id, "Mean Imputation - Mean per player")  
+  tab2 = get_params(fit.mean.week, "Mean Imputation - Mean per week")  
+  tab3 = get_params(fit.reg, "Regression Imputation")  
+  tab4 = get_params(fit.pmm, "Multiple Imputation", pool = TRUE)  
+  tab5 = get_params(fit.cc, "Complete Case Analysis")  
+  
+  d_fits = bind_rows(target_param, tab1, tab2, tab3, tab4, tab5)
+  d_fits = d_fits %>% mutate(rep = rep)
+  d_fits
+}
+
+# we create fake injuries
+d_srpe = d_srpe %>% 
+  mutate(inj_prop = inj_probability_srpe(srpe), 
+         injury = rbinom(length(inj_prop), 1, prob = inj_prop))
+
+# logistic regression for comparison
+fit.target = glm(injury ~ srpe, family = "binomial", data = d_srpe)
+target_param = get_params(fit.target, "No Imputation")  
+
+# remove variables we in theory wouldn't know about
+# in a real life situation
+d_exdata_srpe = d_srpe %>% dplyr::select(-inj_prop, -srpe)
+
+# for missing at random, we create fake variables with correlation to the amount of missing
+# we add fake age and sex, and we use the day of the week to determine weekend
+# we'll use these variables to create Missing at Random
+srpe_p_id = d_srpe %>% distinct(p_id) 
+srpe_id_base = srpe_p_id %>% mutate(age = sample(18:30, length(srpe_p_id$p_id), replace = TRUE),
+                                    sex = sample(0:1, length(srpe_p_id$p_id), replace = TRUE))
+d_exdata_mar = d_exdata_srpe %>% 
+  left_join(srpe_id_base, by = "p_id") %>% 
+  mutate(freeday = ifelse(mc_day == "M+1" | mc_day == "M+2", 1, 0),
+         match = ifelse(mc_day == "M", 1, 0))
+
+# this is what will go in the for-loop:
+d_mcar_80 = add_mcar_rpe(d_exdata_srpe, 0.8)
+sim_impfit(d_mcar_80, target_param, 1)
+d_mar1 = add_mar_rpe(d_exdata_mar, "light")
+sim_impfit(d_mar1, target_param, 1)
+
+
+
+#------------------------------------------------------Step-by-step process of the simulation below-------------------------------------------------
+# Either for understanding or troubleshooting, 
+# the functions above are broken down step-by-step
+# below
 
 #------------------------------------------Step 1 add fake injuries with known relationship with load variable
 
@@ -194,78 +365,6 @@ tab4 = get_params(fit.pmm, "Multiple Imputation", pool = TRUE)
 tab5 = get_params(fit.cc, "Complete Case Analysis")  
 
 d_fits = bind_rows(target_param, tab1, tab2, tab3, tab4, tab5)
-
-#---------------------------------------------Step 7 create function that performs step 3 to 6 a user-chosen number of times
-sim_impfit = function(d_missing, target_param, rep = 1){
-
-  #-----------------impute using all the different methods
-  # Mean imputation by the mean per player
-  d.mean.p_id = impute_mean(d_missing_srpe20, p_id)
-  
-  # Mean imputation by the mean per week
-  d.mean.week = impute_mean(d_missing_srpe20, week_nr)
-  
-  # Regression imputation
-  mids.reg = mice(d_missing, method = "norm.predict", seed = 1234, m = 1, print = FALSE)
-  d.reg = mice::complete(mids.reg, "long", include = FALSE) %>% mutate(srpe = rpe*duration)
-  
-  # Multiple imputation with predicted mean matching, and the Impute, then transform strategy
-  mids.pmm = mice(d_missing, print = FALSE, seed = 1234)
-  imp.pmm = mice::complete(mids.pmm, "long", include = TRUE)
-  imp.pmm$srpe = with(imp.pmm, rpe*duration)
-  mids.itt.pmm = as.mids(imp.pmm)
-  
-  # complete case analysis
-  d.cc = na.omit(d_missing) %>% mutate(srpe = rpe*duration)
-  
-  # fit our models
-  fit.mean.p_id = fit_glm(d.mean.p_id)
-  fit.mean.week = fit_glm(d.mean.week)
-  fit.reg =  fit_glm(d.reg)
-  fit.pmm =  with(mids.itt.pmm, glm(injury ~ srpe, family = binomial))
-  fit.cc =  fit_glm(d.cc)
-  
-  # fetch model parameters
-  tab1 = get_params(fit.mean.p_id, "Mean Imputation - Mean per player")  
-  tab2 = get_params(fit.mean.week, "Mean Imputation - Mean per week")  
-  tab3 = get_params(fit.reg, "Regression Imputation")  
-  tab4 = get_params(fit.pmm, "Multiple Imputation", pool = TRUE)  
-  tab5 = get_params(fit.cc, "Complete Case Analysis")  
-  
-  d_fits = bind_rows(target_param, tab1, tab2, tab3, tab4, tab5)
-  d_fits = d_fits %>% mutate(rep = rep)
-  d_fits
-}
-
-# we create fake injuries
-d_srpe = d_srpe %>% 
-  mutate(inj_prop = inj_probability_srpe(srpe), 
-         injury = rbinom(length(inj_prop), 1, prob = inj_prop))
-
-# logistic regression for comparison
-fit.target = glm(injury ~ srpe, family = "binomial", data = d_srpe)
-target_param = get_params(fit.target, "No Imputation")  
-
-# remove variables we in theory wouldn't know about
-# in a real life situation
-d_exdata_srpe = d_srpe %>% dplyr::select(-inj_prop, -srpe)
-
-# for missing at random, we create fake variables with correlation to the amount of missing
-# we add fake age and sex, and we use the day of the week to determine weekend
-# we'll use these variables to create Missing at Random
-srpe_p_id = d_srpe %>% distinct(p_id) 
-srpe_id_base = srpe_p_id %>% mutate(age = sample(18:30, length(srpe_p_id$p_id), replace = TRUE),
-                                    sex = sample(0:1, length(srpe_p_id$p_id), replace = TRUE))
-d_exdata_mar = d_exdata_srpe %>% 
-  left_join(srpe_id_base, by = "p_id") %>% 
-  mutate(freeday = ifelse(mc_day == "M+1" | mc_day == "M+2", 1, 0),
-         match = ifelse(mc_day == "M", 1, 0))
-
-# this is what will go in the for-loop:
-d_mcar_80 = add_mcar_rpe(d_exdata_srpe, 0.8)
-sim_impfit(d_mcar_80, target_param, 1)
-d_mar1 = add_mar_rpe(d_exdata_mar, "light")
-sim_impfit(d_mar1, target_param, 1)
 
 #----------------------------------------Same for Total distance
 
