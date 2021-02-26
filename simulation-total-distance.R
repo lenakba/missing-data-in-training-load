c5a2b9baf10109b256ec8b9acbf2232a8af19057
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-02-26 12:27:57 +0100

    Removed sRPE from total distance analyses, simulating a study that only gathered GPS measures.

--- simulation-total-distance.R
+++ simulation-total-distance.R
@@ -18,7 +18,7 @@ d_td_full = read_delim(paste0(folder_data, "norwegian_premier_league_football_td
 # select vars we need in the simulation, key variables we think are correlated with the level of total distance
 keyvars = c("p_id", "training_date", "mc_day", "week_nr")
 d_td = na.omit(d_td_full) %>% 
-       select(all_of(keyvars), td = total_distance_daily, srpe, v4 = v4_distance_daily, v5 = v5_distance_daily, pl = player_load_daily)
+       select(all_of(keyvars), td = total_distance_daily, v4 = v4_distance_daily, v5 = v5_distance_daily, pl = player_load_daily)
 
 # adding a variable for match
 # under the assumption that this is very predictive of total distance
