b6acca3d7a87937dbc110a0df593617aeab95500
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-03-04 11:56:50 +0100

    Calculating study characterstics, and reporting of missing data.

--- missing-in-training-load-injury-studies.R
+++ missing-in-training-load-injury-studies.R
@@ -6,25 +6,58 @@ library(readxl) # for reading .xlsx files
 library(lmisc) # ggplot2 themes etc.
 library(devEMF) # to save emf files
 
+#-----------------------------------------------------Data preparation
+
 # reading data
 folder_data = paste0("O:\\Prosjekter\\Bache-Mathiesen-002-missing-data\\Data\\")
 d_tl_study_full = read_excel(paste0(folder_data, "studies_missing_reporting.xlsx"))
 
 # selecting the variables needed for these analyses
 key_vars = c("study_id", "author_first", "year", "title")
-base_vars = c("sport", "n_athletes", "population_sex", "population_mean_age", "n_injuries", "load_variable")
-d_tl_study = d_tl_study_full %>% select(all_of(key_vars), all_of(base_vars), starts_with("missing"))
+base_vars = c("sport", "population_sex", "population_mean_age", "load_variable", "access")
+d_tl_study_selected = d_tl_study_full %>% select(all_of(key_vars), all_of(base_vars), starts_with("study_length"), starts_with("n_"), starts_with("missing"), starts_with("review_"))
+
+# filtering on inclusion criteria
+# has access
+d_tl_study_access = d_tl_study_selected %>% filter(access == "Yes")
+
+# study year >= 2010
+d_tl_study_recent = d_tl_study_access %>% filter(year >= 2010)
+
+# study is in either of the Reviews, or is study year >= 2019
+d_tl_study = d_tl_study_recent %>% filter_at(vars(starts_with("review")), any_vars(. == "Yes" | year >= 2019))
+
+#-------------------------------------------- Study characteristics
+# percentage that reported the number of injuries in the study
+d_tl_study %>% summarise(n_report = sum(!is.na(n_injuries_analyses), na.rm = TRUE), n_studies = n(), prop = n_report/n_studies)
+d_tl_study %>% summarise(mean_age = mean(population_mean_age, na.rm = TRUE), sd_age = sd(population_mean_age, na.rm = TRUE))
+
+calc_perc = function(x, d = d_tl_study){
+  x = enquo(x)
+  d %>% count(!!x) %>% mutate(denominuator = sum(n), prop = n/denominuator) %>% arrange(desc(prop))
+}
+
+calc_perc(load_variable)
+calc_perc(population_sex)
+calc_perc(sport)
+
+d_tl_study = d_tl_study %>% mutate(study_length_cat = ifelse(study_length_metric == "season" | study_length_metric == "year" | study_length_metric == "school year", "season/year/school year", "Other metrics"))
+d_tl_study %>% group_by(study_length_cat) %>% calc_perc(study_length_n, .)
+#-------------------------------------------- Missing data reporting
 
 # percentage that reported missing in the training load variable
-d_tl_study %>% summarise(n_report = sum(missing_reported == "Yes", na.rm = TRUE), n_studies = n(), prop = n_report/n_studies)
+calc_perc(missing_reported_in_tl_variable)
 
 # mean amount of missing
-missing_n_vars = c("missing_load_perc", "missing_injury_perc", "missing_players")
+missing_n_vars = c("missing_load_perc", "missing_injury_perc", "missing_elligble_players_perc")
 d_tl_study %>% summarise_at(vars(all_of(missing_n_vars)), ~mean(., na.rm = TRUE))
 d_tl_study %>% summarise_at(vars(all_of(missing_n_vars)), ~sd(., na.rm = TRUE))
 
 # how many report how much missing?
-d_tl_study %>% summarise(sum(!is.na(missing_load_perc)))
+d_tl_study %>% summarise(n_how_much_missing = sum(!is.na(missing_load_perc)), n_studies = n(), prop = n_how_much_missing/n_studies)
+d_tl_study %>% summarise(n_how_much_missing_player = sum(!is.na(missing_elligble_players_perc)), n_studies = n(), prop = n_how_much_missing_player/n_studies)
+d_tl_study %>% summarise(n_how_much_missing_inj = sum(!is.na(missing_injury_perc)), n_studies = n(), prop = n_how_much_missing_inj/n_studies)
+
 
 # function for making a dot plot for a consistent style
 fig_dots = function(d, x, y, title, percent = FALSE){
@@ -46,7 +79,7 @@ fig_dots = function(d, x, y, title, percent = FALSE){
 
 # Massaging data for a figure showing what missing imputation methods publications choose
 # Remove 1 publication that dutifully reported not having any missing data (they don't need methods to handle them then, do they?)
-d_missing = d_tl_study %>% filter(missing_reported == "Yes", study_id != 8)
+d_missing = d_tl_study %>% filter(missing_reported_in_tl_variable == "Yes", study_id != 8)
 
 d_missing_methods = d_missing %>% 
   count(missing_method) %>% 
@@ -54,6 +87,8 @@ d_missing_methods = d_missing %>%
   arrange(prop) %>% 
   mutate(mmethod_fct = fct_inorder(missing_method))
 n_missing = nrow(d_missing)
+
+d_missing_methods %>% arrange(desc(prop))
 
  emf("n_missing_methods.emf", width = 6, height = 3)
 fig_dots(d_missing_methods, prop, mmethod_fct, paste0("Methods used for handling missing data (n = ",n_missing,")"), percent = TRUE)
