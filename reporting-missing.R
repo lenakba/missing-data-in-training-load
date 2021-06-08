aca4fff9e436ecc498c4de375ba9718a41156603
Lena Kristin Bache-Mathiesen <lenakba@users.noreply.github.com>
2021-06-08 08:43:35 +0200

    Removed local objects and ggplot2 themes
    To make the code useable by the general public.

--- reporting-missing.R
+++ reporting-missing.R
@@ -10,7 +10,7 @@ options(scipen = 17,
         stringsAsFactors = FALSE)
 
 # reading data
-folder_data = paste0("O:\\Prosjekter\\Bache-Mathiesen-002-missing-data\\Data\\")
+folder_data = paste0("my\\data\\folder\\")
 
 # note that the RPE data is per session (which there can be multiple of per day)
 # and that the Total Distance data is per day
@@ -23,13 +23,11 @@ keyvars = c("p_id", "training_date", "mc_day", "week_nr")
 d_srpe = d_rpe_full %>% filter(!is.na(rpe) & !is.na(duration)) %>% select(all_of(keyvars), rpe, duration) %>% mutate(srpe = rpe*duration)
 
 # calculating missing data
-
 # number of players
 n_distinct(d_td_full$p_id)
 
 # number of players that did not have total distance data
 d_td_full %>% distinct(p_id, .keep_all = TRUE) %>% summarise(missing_player = sum(missing_player == 1))
-
 d_td_full %>% count(missing_td, missing_td_text)
 
 # number of players that did not have RPE data
@@ -63,33 +61,18 @@ td_values = d_td$gps_td
 # distributions of load values
 plot_hist_srpe = 
   ggplot(enframe(srpe_values), aes(x = srpe_values)) + 
-  geom_histogram(fill = nih_distinct[4]) + 
-  theme_base(13) + 
+  geom_histogram() +
   xlab("sRPE (AU)") + ylab("Count")  +
   scale_y_continuous(expand = expansion(mult = c(0.0, 0.06), add = 0)) +
   theme(panel.grid = element_blank(),
-        axis.line = element_line(color = nih_distinct[4]),
-        axis.ticks = element_line(color = nih_distinct[4]),
         panel.border = element_blank(), 
         panel.background = element_blank())
 
 plot_hist_td = 
   ggplot(enframe(td_values), aes(x = td_values)) + 
-  geom_histogram(fill = nih_distinct[4]) + 
-  theme_base(13) + xlab("Total Distance (m)")  + ylab("Count") + 
+  geom_histogram() + 
+  xlab("Total Distance (m)")  + ylab("Count") + 
   scale_y_continuous(expand = expansion(mult = c(0.0, 0.06), add = 0)) + 
   theme(panel.grid = element_blank(),
-        axis.line = element_line(color = nih_distinct[4]),
-        axis.ticks = element_line(color = nih_distinct[4]),
         panel.border = element_blank(), 
         panel.background = element_blank())
-
-emf("srpe_td_distribution.emf", width = 10, height = 4.5)
-ggpubr::ggarrange(plot_hist_srpe, plot_hist_td, ncol = 2, labels = c("a", "b"))
-dev.off()
-
-d_load_full = read_delim(paste0(folder_data, "norwegian_premier_league_football_anon.csv"), delim = ";")
-
-# missing can now be calculated
-d_load_full %>% count(missing_td, missing_td_text)
-d_load_full %>% count(missing_load, missing_load_text)
